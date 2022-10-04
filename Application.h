a6225277c7ce79adb93ce0d320ba9b07a524ee1b
Stanislav <72697394+SgXXG@users.noreply.github.com>
2022-10-04 19:04:51 +0300

    made

--- Application.h
+++ Application.h
@@ -1,1 +1,24 @@
 #pragma once
+#include "Core.h"
+#include "ApplicationWindow.h"
+
+namespace CPPSnake {
+	
+	class Application {
+
+	public:
+
+		Application() {}
+		~Application();
+
+		Bool initialize();
+		Void run();
+		Void quit();
+
+	private:
+
+		Void processFrame();
+	};
+
+	extern Application* _app;
+}
