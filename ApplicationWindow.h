9b94284e2fe8ef56cf56fe1235e12f6b4ec1e090
Stanislav <72697394+SgXXG@users.noreply.github.com>
2022-10-04 16:03:36 +0300

    changes

--- ApplicationWindow.h
+++ ApplicationWindow.h
@@ -1,1 +1,33 @@
 #pragma once
+#include "Core.h"
+#include <windows.h>
+
+namespace CPPSnake {
+	class ApplicationWindow {
+
+		friend class Application;
+
+	private:
+
+		ApplicationWindow() {};
+		~ApplicationWindow();
+
+	public:
+
+		HWND getHandle() const { return _handle;  }
+		HDC hetClientDC() const { return _clientDC; }
+		Size2UI32 getClientSize() const { return _clientSize; }
+		UInt32 getClientWidth() const { return _clientSize.width; }
+		UInt32 getClientHeight() const { return _clientSize.height; }
+
+
+	private:
+
+		HWND _handle{};
+		HDC _clientDC{};
+		Size2UI32 _clientSize{};
+
+	};
+
+	extern ApplicationWindow* _appWindow;
+}
